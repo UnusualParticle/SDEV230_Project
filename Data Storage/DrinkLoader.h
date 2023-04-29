3aac6923e83bd225ad3673c41855474c2ae24b49
GambitAnimating <lumarigamesdev@gmail.com>
2023-04-29 16:19:45 -0400

    Started implementing data storage for drinks

---
+++ Data Storage/DrinkLoader.h
@@ -0,0 +1,11 @@
+#ifndef SDEV230_PROJECT_DRINKLOADER_H
+#define SDEV230_PROJECT_DRINKLOADER_H
+
+
+class DrinkLoader {
+public:
+    void LoadDrinks();
+};
+
+
+#endif //SDEV230_PROJECT_DRINKLOADER_H
--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@
 #include "UI/UserInterface.h"
 #include "UI/UI_Menu.h"
 #include "UI/Styles.h"
+#include "Data Storage/DrinkLoader.h"
 
 // Styles.cpp is not implimented yet
 void LoadStyles()
@@ -13,10 +14,18 @@ void LoadStyles()
 	LoadStyle("background", "Data/style_background.json");
 }
 
+void TestDrinks()
+{
+    // put code here
+    DrinkLoader loader;
+    loader.LoadDrinks();
+}
+
 int main()
 {
 	// put code here
 	std::cout << "\n== Hello World! ==\n";
+    TestDrinks();
 	//SFML_TEST();
 	//ui::TEST_UI::RunTest();
 	ui::TEST_MENU::RunTest();
